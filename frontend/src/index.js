9041d716669ed986a977917ec6440c07344a6bff
pnhalim <pnhalim@umich.edu>
2024-01-20 15:36:11 -0500

    asdfgndsadfs

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./App.css";
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
