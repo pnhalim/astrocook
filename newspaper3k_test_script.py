fb46d94425e030f134ebbf6073d6419b87630235
Nathan Yap <nathancyap@gmail.com>
2024-01-20 11:15:33 -0500

    article to json

--- newspaper3k_test_script.py
+++ newspaper3k_test_script.py
@@ -1,10 +1,31 @@
 from newspaper import Article
+import json
 
-url = "https://www.allrecipes.com/recipe/83549/angelas-awesome-enchiladas/"
-article = Article(url)
+# url = "https://www.allrecipes.com/recipe/83549/angelas-awesome-enchiladas/"
+# article = Article(url)
 
-article.download()
-article.parse()
-print(article.html)
-with open("test.html", "w+") as file:
-    file.write(article.html)
+# article.download()
+# article.parse()
+# print(article.html)
+# with open("test.html", "w+") as file:
+#     file.write(article.html)
+
+with open("test.html", "r") as file:
+    html_doc = file.read()
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+
+allrecipe_metadata = soup.find(id="allrecipes-schema_1-0")
+with open("test_metadata", "w+") as file:
+    file.write(allrecipe_metadata.string)
+try:
+    metadata_json = json.loads(allrecipe_metadata.string)
+    with open("output_json", "w+") as outfile:
+        json.dump(metadata_json, outfile)
+except Exception as e:
+    print("Couldn't parse json {}".format(e))
+
+
+
