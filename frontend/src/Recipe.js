37c67ecf548652ca53f547d76eb21612c4770e78
pnhalim <pnhalim@umich.edu>
2024-01-20 16:06:22 -0500

    asdf

---
+++ frontend/src/Recipe.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Recipe = () => {
+
+    hi, setHi = useState(0)
+
+    return (
+        <div>
+            <h1>Recipe Found!</h1>
+        </div>
+    );
+};
+
+export default Recipe;
